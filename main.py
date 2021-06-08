849bbd3af0c5f08316a11e9988d3abc7ca7fd2aa
Flavian Anselmo <>
2021-06-08 14:58:12 +0000

    time complexity

---
+++ main.py
@@ -0,0 +1,22 @@
+def countdown(n):
+    while n > 0:
+        print(n)
+        n -= 1	
+    print("Blast off!") 
+countdown(5)
+
+"""
+TIME COMPLEXITY 
+#O(n)the loop  runs in linear time 
+#O(1)the time comlexity is in constant time 
+#O(n)this runs in linear time 
+#the print function runs in constant time 
+there fore the time complexity of this algorithm is O(n)+O(1)+O(n)
+the constant time is canceled 
+the time complexity is IN QUADRATIC TIME O(n^2)
+
+SPACE COMLEXITY
+ 
+
+
+"""
