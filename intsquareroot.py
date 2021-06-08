c22e3e1f0e64180993dd9a0e3652f8bf6b744085
Flavian Anselmo <>
2021-06-08 18:26:58 +0000

    sqrt

---
+++ intsquareroot.py
@@ -0,0 +1,19 @@
+def int_sqrt(x):
+    for i in range(x):
+        if i * i == x:
+            return i
+    
+    return -1
+"""
+TIME COMLEXITY 
+-------------
+the loop runs in linear time 
+the if statement is done in constant time 
+with respect to the loop that runs in linear time O(n)
+there for the timw complexity for the program is O(n)
+
+SPACE COMLEXITY 
+---------------
+The memory allocation is is conatant since initalisation occurs once 
+hence the space comlexit is O(1)
+"""
